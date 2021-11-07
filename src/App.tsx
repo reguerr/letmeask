49567817e24b2330d6e76a2f4d22ba6696b64f2e
Renata Guerra <re.guerr@gmail.com>
2021-11-07 17:17:36 -0300

    Added admin room

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { Room } from './pages/Room';
+import { AdminRoom } from './pages/AdminRoom';
 
 import { AuthContextProvider } from './contexts/AuthContext';
 
@@ -13,7 +14,9 @@ function App() {
         <Switch>
           <Route path="/rooms/new" component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
           <Route path="/" component={Home} />
+
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
